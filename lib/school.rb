984fb9a308ce300bb7d6a2bc7d35662801b72fbe
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-28 00:12:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,15 +13,11 @@ end
 
 
 def sort
-sorted_array_roster = @roster.sort
-sorted_array_roster.collect do |grades_with_names|
-  grades_with_names.collect do |element|
-    if element.kind_of?(Array)
-      element = element.sort!
-    end 
-  end 
-end
-sorted_roster = sorted_array_roster.to_h
+  sorted = {}
+@roster.collect do |grade, students|
+  sorted[grade] = students.sort
+end 
+sorted
 end 
 
 def add_student(student_name, grade)
